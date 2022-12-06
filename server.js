356107b9e489e29ea3edfea27b743fde2f73d36b
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2022-12-06 19:35:47 +0200

    control order of operations

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 const localSite = "http://127.0.0.1:5500/Lieutenant%20Governor's%20Office%20-%20Public%20Search.html";
 const disclosureSite = "https://disclosures.utah.gov/Search/PublicSearch";
 
-const loadLinkIdList = ['ELECT'] //['PCC', 'CORP', 'ELECT', 'INDEXP', 'LABOR', 'PAC', 'PIC', 'PARTY'];
+const loadLinkIdList = ['PCC', 'CORP', 'ELECT', 'INDEXP', 'LABOR', 'PAC', 'PIC', 'PARTY'];
 
 async function scrape(loadLinkId, browser) {
   const page = await browser.newPage();
@@ -31,7 +31,7 @@ async function scrape(loadLinkId, browser) {
         return a.href
       })
     });
-    // page.close();
+    page.close();
     return await refList;
   }
 }
@@ -43,46 +43,48 @@ async function downloadCsv(fileUrl, browser) {
   page.$eval('.dis-csv-list', el => {
     el.children[0].children[0].click()
   })
-  // await page.close()
+  await page.close()
 }
 
-async function saveCsvUrls() {
-  const browser = await puppeteer.launch({ headless: false });
+function runSraper() {
+  const browser = puppeteer.launch({ headless: false });
   loadLinkIdList.forEach(async (linkId, index) => {
+
     const result = await scrape(linkId, await browser);
+
     const filtered = result.filter(href => {
       return href.includes('https://disclosures.utah.gov/Search/PublicSearch/FolderDetails/')
     })
+    //store url arrays in json files
     let data = JSON.stringify(filtered);
     fs.writeFileSync(`links/${linkId}.json`, data)
-    console.log(`saving report urls: ${index} / ${loadLinkIdList.length}`);
+    console.log(`storing report urls: ${index} / ${loadLinkIdList.length}`);
+
+    //download csv files
+    getData(linkId, await browser)
+    console.log(`getting csv files: ${index} / ${loadLinkIdList.length}`);
   });
   return true
-  
 }
 
 //download files
-async function getData() {
-  const browser2 = await puppeteer.launch({ headless: false });
-  loadLinkIdList.forEach((linkId, index) => {
+function getData(linkId, browser) {
     fs.readFile(`links/${linkId}.json`, (err, data) => {
       const linkList = JSON.parse(data);
-      
-      linkList.forEach(link => {
-        downloadCsv(link, browser2)
+      linkList.forEach(async link => {
+        downloadCsv(link, await browser)
       })
-    })
+
   })  
 }
 
-async function runProgram() {
-  const save = await saveCsvUrls()
-  if (await save) {
-    getData()
-  }
-}
 
-runProgram();
+
+runSraper();
+
+
+
+
 
 
 app.listen(port, () => {
