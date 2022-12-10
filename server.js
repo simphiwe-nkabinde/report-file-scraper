197debdcafbab805606d20b69253d06612400e30
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2022-12-10 16:02:36 +0200

    fix bugs. dowloading csv files

--- server.js
+++ server.js
@@ -1,91 +1,85 @@
-const { request, response } = require("express");
 const express = require("express");
 const puppeteer = require("puppeteer");
-const fs = require('fs');
-const res = require("express/lib/response");
 const app = express();
 const port = 3000;
-
-app.get("/", (req, res) => {
-  console.log("request");
-  res.send("Hello World!");
-  scrape();
-});
 
 const localSite = "http://127.0.0.1:5500/Lieutenant%20Governor's%20Office%20-%20Public%20Search.html";
 const disclosureSite = "https://disclosures.utah.gov/Search/PublicSearch";
 
 const loadLinkIdList = ['PCC', 'CORP', 'ELECT', 'INDEXP', 'LABOR', 'PAC', 'PIC', 'PARTY'];
 
+
+/**
+ * 
+ * @param {string} loadLinkId 
+ * @param {Promise<puppeteer.Browser>} browser 
+ * @returns {string[]} list of urls
+ */
 async function scrape(loadLinkId, browser) {
   const page = await browser.newPage();
+  try {
+    await page.goto(disclosureSite);
+    await page.click(`#${loadLinkId}`);
 
-  await page.goto(disclosureSite);
-  await page.click(`#${loadLinkId}`);
-
- const searchList = await page.waitForSelector(`legend`, {visible: true})
- let refList;
-  if (await searchList) {
-    refList = await page.$$eval(`a`, aTags => {
-      return aTags.map(a => {
-        return a.href
-      })
-    });
+    const searchList = await page.waitForSelector(`legend`, { visible: true });
+    let refList;
+    if (await searchList) {
+      refList = await page.$$eval(`a`, (aTags) => {
+        return aTags.map((a) => {
+          return a.href;
+        });
+      });
+      page.close();
+      return await refList;
+    }
+  } catch (err) {
+    console.log(`FAIL: scrape() (${loadLinkId}))`, err.message);
     page.close();
-    return await refList;
   }
 }
 
+/**
+ * 
+ * @param {string} fileUrl 
+ * @param {Promise<puppeteer.Browser>} browser 
+ */
 async function downloadCsv(fileUrl, browser) {
   const page = await browser.newPage();
-
-  await page.goto(fileUrl);
-  page.$eval('.dis-csv-list', el => {
-    el.children[0].children[0].click()
-  })
-  await page.close()
+  try {
+    await page.goto(fileUrl);
+    page.$eval(".dis-csv-list", (el) => {
+      el.children[0].children[0].click();
+    });
+    await page.close();
+  } catch (err) {
+    console.log("downloadCvs Error:", err.message);
+    await page.close();
+  }
 }
 
 function runSraper() {
-  const browser = puppeteer.launch({ headless: false });
+  const browser = puppeteer.launch({ headless: true });
   loadLinkIdList.forEach(async (linkId, index) => {
-
-    const result = await scrape(linkId, await browser);
-
-    const filtered = result.filter(href => {
-      return href.includes('https://disclosures.utah.gov/Search/PublicSearch/FolderDetails/')
-    })
-    //store url arrays in json files
-    let data = JSON.stringify(filtered);
-    fs.writeFileSync(`links/${linkId}.json`, data)
-    console.log(`storing report urls: ${index} / ${loadLinkIdList.length}`);
-
-    //download csv files
-    getData(linkId, await browser)
-    console.log(`getting csv files: ${index} / ${loadLinkIdList.length}`);
+    try {
+      console.log(
+        `START: ID ${index + 1} of ${loadLinkIdList.length} (${linkId}))`
+      );
+      const result = await scrape(linkId, await browser);
+      if (await result) {
+        await result.forEach(async (href, index) => {
+          if (href.includes("https://disclosures.utah.gov/Search/PublicSearch/FolderDetails/")) {
+            downloadCsv(href, await browser);
+          }
+        });
+      }
+    } catch (err) {
+      console.log(`FAILED: ID ${index + 1} of ${loadLinkIdList.length} (${linkId}))`, err);
+    }
   });
-  return true
+  return true;
 }
-
-//download files
-function getData(linkId, browser) {
-    fs.readFile(`links/${linkId}.json`, (err, data) => {
-      const linkList = JSON.parse(data);
-      linkList.forEach(async link => {
-        downloadCsv(link, await browser)
-      })
-
-  })  
-}
-
-
 
 runSraper();
-
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
